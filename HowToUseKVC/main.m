7e2eaa1a6562e56a9fa3c5e1c68652460829f85e
何知昶 <foreversun@zhichanghedemacbook-pro.local>
2018-03-25 15:27:20 +0800

    Initial Commit

---
+++ HowToUseKVC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HowToUseKVC
+//
+//  Created by zhichang.he on 2018/3/25.
+//  Copyright © 2018年 zhichang.he. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
